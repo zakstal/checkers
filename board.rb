e66245f4149be067c3e8929783e5e7aedf6cc8b8
App Academy Student <student@appacademy.io>
2014-09-18 13:46:07 -0400

    show board works, has validation

--- board.rb
+++ board.rb
@@ -3,71 +3,130 @@ require          'colorize'
 
 class Board
 
+  WHITE = [
+    [0, 0],
+    [0, 2],
+    [0, 4],
+    [0, 6],
+
+    [1, 1],
+    [1, 3],
+    [1, 5],
+    [1, 7],
+
+    [2, 0],
+    [2, 2],
+    [2, 4],
+    [2, 6],
+  ]
+  BLACK = [
+    [5, 0],
+    [5, 2],
+    [5, 4],
+    [5, 6],
+
+    [6, 1],
+    [6, 3],
+    [6, 5],
+    [6, 7],
+
+    [7, 0],
+    [7, 2],
+    [7, 4],
+    [7, 6],
+  ]
+
 
   def initialize(populate = true)
-    popluate_board(populate)
+    setup_board(populate)
   end
 
-  def place_piece(pos,color)
-     @rows[pos] = Piece.new(color)
+  def setup_board(populate)
+    @rows = Array.new(8) { Array.new(8)}
+
+    return if populate == false
+    self.fill_rows
   end
 
   def fill_rows
-    [:white,:black].each do |color|
+    color = [:w,:b]
+    [WHITE,BLACK].each_with_index do |all_pos,index|
 
-      [0,2,5,7].each_with_index do |fill_row,fill_cell|
-        pos = fill_row,fill_cel
-          place_piece(pos,color) if fill_cell.even?
+      all_pos.each do |pos|
+        place_piece(pos,color[index])
       end
-
-      [1,6].each_with_index do |fill_row,fill_cell|
-        pos = fill_row,fill_cel
-         place_piece(pos,color) if !fill_cell.even?
-      end
-
     end
   end
 
+  def place_piece(pos,color)
+    y,x = pos
+     @rows[y][x] = Piece.new(pos,color,self)
+  end
+
+
+
   def[](pos)
     y,x = pos
-    @rows[y,x]
+    @rows[y][x]
   end
 
-  def[]=(pos)
+  def[]=(pos,value)
     y,x = pos
-    @rows[y,x]
+    @rows[y][x] = value
   end
 
-  def popluate_board(populate)
-    @rows = Array.new(7) { Array.new(7)}
-    return unless populate == true
+  def move
+    moves = gets.chomp.split("")
+    start = moves.first(2).map(&:to_i)
+    goal = moves.last(2).map(&:to_i)
 
-
+    self[start].valid_move?(goal)
+  # rescue
+ #    puts "Try again"
+ #    retry
+    self[start], self[goal] = self[goal], self[start]
   end
+
 
   def pieces
     @rows.flatten.compact
   end
 
+  def piece_positions
+    pieces.map{|piece| piece.pos}
+  end
+
 
   def render
      @rows.map.with_index do |row,row_index|
+
         render_tiles(row,row_index)
      end.join("\n")
   end
 
   def render_tiles(row,row_index)
-    color = row_index.even? ? 1 : 0
+    swich = row_index.even? ? 1 : 0
     row.map.with_index do |cell,cell_index|
-      (cell_index + color).even? ? "   ".colorize(:background => :white) : "   "
-     end.join("")
+
+        render_tile(cell_index,row_index, swich)
+    end.join("")
   end
+
+
+  def render_tile(cell_index,row_index, swich)
+
+    cell = (self[[row_index,cell_index]].nil? ? "   " : " #{self[[row_index,cell_index]].color} ")
+    (cell_index + swich).even? ? cell.colorize(:background => :white) : cell
+  end
+
 
   def show
     puts render
   end
 
+  def out_of_bound?(goal_pos)
 
+  end
 
 end
 
--- piece.rb
+++ piece.rb
@@ -3,38 +3,84 @@ class Piece
 
   attr_accessor :king, :color, :pos, :board
 
-  def initialize(pos, color)
-    @king = false
-    @pos = pos
-    @color = (:w ? :w : :b)
-    @delta = [
+  DELTAS = [
       [-1,-1],
       [-1, 1],
       [ 1,-1],
       [ 1, 1]
     ]
+
+  def initialize(pos, color,board)
+    @king   = false
+    @board  = board
+    @pos    = pos
+    @color  = (color == :w ? :w : :b)
+    @delta  = (color == :w ? DELTAS.last(2) : DELTAS.first(2) )
   end
 
   def promote(pos)
-    self.King = true if pos[y] == (pos[y] ? 0 : 7)
+    self.King = true if pos[y] == 7 && self.color == :w
+    self.King = true if pos[y] == 0 && self.color == :b
+    @delta = DELTAS
   end
 
-  def forward?(goal_pos)
-    new_pos = color == :w ? pos[0] - 1 : pos[0] + 1
-
-    y, x = forward_move
-    (y + new_pos) == (goal_pos)
+  # def forward?(goal_pos)
+#     # white moves up the board or  -1
+#     # black moves down the board or 1
+#     new_pos = (color == :w ? pos[0] - 1 : pos[0] + 1)
+#
+#     y, x = forward_move
+#     raise "Worng direction" if (y + new_pos) >= (goal_pos)
+#     true
+#   end
+  def on_board?(pos)
+    pos.all?{|n| n.between?(0,7)}
   end
 
-  def non_king_moves(goal_pos)
 
+  def grow_moves(times)
+    moves = []
+    @delta.each do |delta|
+      temp = [pos]
+      times.times do |i|
+        dy,dx = delta
+        sy,sx = temp.last
+
+        move = [dy + sy, dx + sx]
+        temp << move if on_board?(move)
+      end
+      moves << temp
+    end
+    moves.flatten(1).uniq
   end
+
+  def empty?(move)
+    board[move].nil?
+  end
+
+  def jump_move(goal_pos)
+    moves     = grow_moves(2)
+    valid     = moves.include?(goal_pos)
+    py,px     = self.pos
+    gy,gx     = goal_pos
+    inbetween = [py + gy, px + gx]
+    valid     = !self.board[inbetween].nil? && moves.includ?(goal_pos)
+    raise "Yout cant move there" if !valid
+  end
+
+  def step_move(goal_pos)
+    moves = grow_moves(2)
+    moves.include?(goal_pos)
+  end
+
 
   def valid_move?(goal_pos)
+    raise "That space is off the board" if !on_board?(pos)
+    raise "That space is occupoied" if !empty?(goal_pos)
+
+
 
 
   end
-
-
 
 end
